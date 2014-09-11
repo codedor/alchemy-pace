c6757c980d8ffbb2050b819db36fd1c749789d2c
Jelle De Loecker <jelle@codedor.be>
2014-09-11 16:10:57 +0200

    Setup new branch for alchemy v0.2

--- bootstrap.js
+++ bootstrap.js
@@ -2,37 +2,10 @@
 var scriptpath = '/public/pace/pace.js',
     csspath    = '/public/pace/pace-flash.css';
 
-alchemy.sputnik.beforeSerial('startServer', function(callback) {
+// Always load the Pace script & style
+alchemy.hawkejs.on({type: 'viewrender', status: 'begin', client: false}, function onBegin(viewRender) {
 
-	// Make sure the i18n drone runs
-	alchemy.hawkejs.afterPayload(function(next, payload) {
-		
-		// Only do this on new pages
-		if (!payload.hawkejs.ajax) {
+	viewRender.script(scriptpath);
+	viewRender.style(csspath);
 
-			// Make sure the file isn't already loaded
-			if (!payload.request.files[scriptpath]) {
-				payload.request.files[scriptpath] = true;
-			
-				// Create an alias to the scripts object
-				var s = payload.request.tags;
-				
-				s[scriptpath] = {
-					type: 'script',
-					path: scriptpath,
-					destination: 'head'
-				};
-
-				s[csspath] = {
-					type: 'style',
-					path: csspath,
-					destination: 'head'
-				};
-			}
-		}
-
-		next();
-	});
-
-	callback();
 });
